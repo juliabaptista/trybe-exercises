c7a7927ac51c6f5d142b9bae0118381c6261b962
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-02-24 18:26:02 -0300

    Cria arquivos para resolução do exercicio bloco 4 dia 1

---
+++ bloco_4/dia_1/scripts.js
@@ -0,0 +1,9 @@
+const nome = "Julia Baptista"; 
+const birthCity = "Belo Horizonte";
+let birthYear = 1989;
+birthYear = 2020
+
+
+console.log(nome);
+console.log(birthCity);
+console.log(birthYear);
